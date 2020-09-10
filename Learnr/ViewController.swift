ff00b9a6cb4298f400e1b82e6dae5baf26b5f2b7
sawzedong <saw.zedong@dhs.sg>
2020-09-10 09:07:24 +0800

    Initial Commit

---
+++ Learnr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learnr
+//
+//  Created by Shaw Sheng Saw on 10/9/20.
+//  Copyright © 2020 Techiaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
