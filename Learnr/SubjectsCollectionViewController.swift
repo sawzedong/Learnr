3569ae5f27ff0197701b42d35020389e618b615d
Vicky <ang.xinen.vicky@dhs.sg>
2020-09-18 18:19:00 +0800

    Added rounded edges to collection view cells; edited icons a little.

--- Learnr/SubjectsCollectionViewController.swift
+++ Learnr/SubjectsCollectionViewController.swift
@@ -20,14 +20,29 @@ class SubjectsCollectionViewController: UIViewController, UICollectionViewDataSo
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! SubjectsCollectionViewCell
         cell.subjectLabel.text = self.items[indexPath.row]
         cell.backgroundColor = UIColor(red: CGFloat.random(in: 0.5...1), green: CGFloat.random(in: 0.5...1), blue: CGFloat.random(in: 0.5...1), alpha: 1)
+        
+        //for shadow and outline - Vicky
+        //cell.layer.shadowColor = UIColor.black.cgColor
+        
+        //cell.layer.shadowOpacity = 0.5
+        
+        //cell.layer.shadowOffset = CGSize(width: 0, height: 5.0)
+        
+        //cell.layer.shadowRadius = 5
+        
+        cell.layer.cornerRadius = 10
+
         return cell
     }
+ 
+    /*var customTabBarItem:UITabBarItem = UITabBarItem(title: nil, image: UIImage(named: "YOUR_IMAGE_NAME")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), selectedImage: UIImage(named: "YOUR_IMAGE_NAME")) */
+    //I added the above as potential code to include custom icons for the icons on the tab bar controller if needed -Vicky
     
     // MARK: - UICollectionViewDelegateFlowLayout
     
     // sets the number of cells per row
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let boundWidth = (collectionView.bounds.width/2.0)-20
+        let boundWidth = (collectionView.bounds.width/2.0)-40
         let boundHeight = boundWidth
 
         return CGSize(width: boundWidth, height: boundHeight)
@@ -35,7 +50,7 @@ class SubjectsCollectionViewController: UIViewController, UICollectionViewDataSo
     
     // sets the margins
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        return UIEdgeInsets(top: 20, left: 25, bottom: 20, right: 25)
     }
     
     // MARK: - UICollectionViewDelegate protocol
